47b1cb9a45bd04a5854633edd4687543e69d3f7e
Sean Henley <seanphenley@gmail.com>
2017-02-26 11:20:21 -0500

    Cleanup/comments, fixed min-ref, added logging, setting up for filters

--- src/wallet/core.clj
+++ src/wallet/core.clj
@@ -1,7 +1,10 @@
 (ns wallet.core
+  (:require [wallet.eve :as eve])
   (:gen-class))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+
--- src/wallet/eve.clj
+++ src/wallet/eve.clj
@@ -1,8 +1,10 @@
 (ns wallet.eve
   "eve api related helpers"
   (:require [clojure.data.xml :as xml]
-            [clojure.string :as str]))
+            [clojure.string :as str]
+            [clojure.tools.logging :as log]))
 
+;; will do this better at some point
 (def api {:keyID "<opsec>"
           :vCode "<opsec>"})
 
@@ -21,17 +23,19 @@
        (str/join "&")))
 
 (defn- tag-contents
+  "Gets the a list of xml data structures for all children that match the tag name"
   [xml tag-name]
   (filter #(= tag-name (:tag %))
           (:content xml)))
 
 (defn- str->num
   "converts a string to a number, returning nil on failure"
-  [str]
-  (let [n (read-string str)]
+  [num-str]
+  (let [n (read-string num-str)]
     (if (number? n) n nil)))
 
 (defn- data-rows
+  "Extracts the row data from the xml, converting to a list of maps of attributes of each row"
   [xml]
   (-> (tag-contents xml :result)
       (first)
@@ -42,50 +46,57 @@
 
 (defn get-transactions
   [start num]
-  (-> (slurp (str eve-api-url "?" (query-params (merge {:fromID start
-                                                        :rowCount num}
-                                                       api))))
-      (xml/parse-str)
-      (data-rows)))
-
+  (let [query-url (str eve-api-url "?" (query-params (merge {:fromID start
+                                                             :rowCount num}
+                                                            api)))]
+    (log/info "Querying API: " query-url)
+    (-> (slurp query-url)
+        (xml/parse-str)
+        (data-rows))))
 
 (defn- min-ref-id
+  "Gets the smallest refID in the current batch of transactions, use to walk backwards"
   [batch]
-  (-> (map #(str->num (:refID %)) batch)
-      (min)))
+  (apply min (map #(str->num (:refID %)) batch)))
 
-(defn get-month-transactions
+(defn get-all-transactions
   "Recursively grab all transactions and merge together"
   []
   (loop [start nil
          transactions '()]
     (let [batch (get-transactions start 2560)
           batch-size (count batch)]
-      (println (str "Batch size: " batch-size))
+      (log/info "Batch size: " batch-size)
+      (log/info "Starting refd: " start)
       (if (= 0 batch-size)
         transactions
         (recur (min-ref-id batch)
                (concat transactions batch))))))
 
+(defn- filter-transactions
+  "filters transactions by one or more parameters"
+  [filters]
+  )
+
 (defn balance-info
   "Takes a list of transactions and returns the sum of all transactions" 
-  []
-  (reduce (fn [acc t]
-            (let [amount (str->num (:amount t))
-                  adder-fn (partial + amount)]
-              (as-> acc res
-                    (update res :delta adder-fn)
-                    (if (> amount 0)
-                      (update res :totalIncome adder-fn)
-                      (update res :totalExpense adder-fn)))))
-          {:delta 0
-           :totalIncome 0
-           :totalExpense 0}
-          (get-month-transactions)))
+  [filters]
+  (->> (get-all-transactions)
+       (reduce (fn [acc t]
+                 (let [amount (str->num (:amount t))
+                       adder-fn (partial + amount)]
+                   (as-> acc res
+                     (update res :delta adder-fn)
+                     (if (> amount 0)
+                       (update res :totalIncome adder-fn)
+                       (update res :totalExpense adder-fn)))))
+               {:delta 0
+                :totalIncome 0
+                :totalExpense 0})))
 
 (defn stations
   "Takes a list of transactions and returns a map of stations and number transactions at each"
-  [transactions]
+  []
   (reduce (fn [stations t]
             (let [station (:argName1 t)]
               (if (string? station)
@@ -94,4 +105,5 @@
                         (fnil inc 0))
                 stations)))
           {}
-          transactions))
+          (get-all-transactions)))
+
